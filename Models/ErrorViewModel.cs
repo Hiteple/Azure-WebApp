f58fec52d68fcf2eba718f19ca080427af595b6f
hiteple <hiteplegold@gmail.com>
2020-06-20 20:03:09 -0300

    Adding project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace platzi_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
